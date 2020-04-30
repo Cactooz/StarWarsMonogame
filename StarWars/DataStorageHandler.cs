8ce517bece82b754c916ad91b283b292cc9c6dfa
Hugo Bachér <huba02001@utb.vaxjo.se>
2020-04-30 14:19:22 +0200

    Added DataStorageHandler for loading and saving data to file

---
+++ StarWars/DataStorageHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace StarWars
+{
+    class DataStorageHandler
+    {
+        private string filePath;
+
+        public DataStorageHandler(string fileName)
+        {
+            filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+        }
+
+        public void Update()
+        {
+
+        }
+
+        private void LoadData()
+        {
+
+        }
+
+        private void SaveData()
+        {
+
+        }
+
+        private void RemoveData()
+        {
+
+        }
+
+        private void WriteData()
+        {
+
+        }
+
+    }
+}
